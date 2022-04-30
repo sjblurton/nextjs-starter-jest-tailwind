6241c6da8600d9ec04244381577d896d8984efa1
Simon Blurton <sblurton@hotmail.co.uk>
2022-04-30 14:00:20 +0100

    initial commit

---
+++ src/pages/index.jsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <div className="container mx-auto flex justify-center items-center h-screen">
+      <h1 className="text-center text-5xl font-bold">
+        Next Js, starter with typescript, jest, and tailwaindcss
+      </h1>
+    </div>
+  );
+}
